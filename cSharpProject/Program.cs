abf06ea431d29b06e8526eaf64ec5b45d38b86a5
RimpyGoyal <rimpy.goyal11@gmail.com>
2021-03-29 00:31:32 -0500

    For loop and If else

--- cSharpProject/Program.cs
+++ cSharpProject/Program.cs
@@ -6,7 +6,23 @@ namespace cSharpProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Hi This is testing");
+            int length = 100;
+            for (int j = 1; j <= length; j++)
+            {
+                Console.Write(j);
+                if (j == 100)
+                {
+                    Console.WriteLine(".");
+                }
+                else
+                {
+                    Console.Write(",");
+                }
+                if (j % 15 == 0)
+                {
+                    Console.Write("\n");
+                }
+            }
         }
     }
 }
