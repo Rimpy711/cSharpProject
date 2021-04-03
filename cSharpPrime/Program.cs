52cc6802b488f9912772c09e42c884de8efa957e
RimpyGoyal <rimpy.goyal11@gmail.com>
2021-04-03 17:48:29 -0500

    Prime Factor Method and Unit Testing

---
+++ cSharpPrime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace cSharpPrime
+{
+    public class Program
+    {
+
+        public static string PrimeFactors(int number)
+        {
+            // F10
+            string res = string.Empty;
+
+            for (int i = 2; i <= number; ++i)
+            {
+               // res = res + "*";
+                while (number % i == 0)
+                {
+                    res = res + i;
+                    number = number / i;
+                    if(i<=number)
+                    res = res + "*";
+
+                }
+            }
+
+            return res;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            // F5
+            Console.WriteLine("Enter a number to calclulate Prime Factor: ");
+            string n1 = Console.ReadLine();
+            int num1 = Int32.Parse(n1);
+
+            Console.WriteLine(PrimeFactors(num1));
+
+            Console.WriteLine("\n\nHave a nice day!");
+
+        
+        }
+    }
+}
+
