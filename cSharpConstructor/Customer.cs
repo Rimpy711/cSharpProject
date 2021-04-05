1c593eaa222cca8df4c379460085c52e6b1b3da3
RimpyGoyal <rimpy.goyal11@gmail.com>
2021-04-05 03:49:56 -0500

    Zoo With IAnimal Interface

---
+++ cSharpConstructor/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpConstructor
+{
+    class Customer
+    {
+        string _firstName;
+        private string _lastName;
+        private static string ClassInfo;
+        static Customer()
+        {
+            ClassInfo = "Our class has name Customer";
+        }
+
+        public static string GetClassInfo() => ClassInfo;
+
+        public Customer(string firstName, string lastName)
+        {
+            Console.WriteLine("ctor with params has been called");
+
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        public Customer() : this("n/a", "n/a")
+        {
+            Console.WriteLine("default ctor has been called");
+
+        }
+        /* public Customer()
+        {
+            _firstName = "n/a";
+            _lastName = "n/a";
+        }*/
+
+        public void PrintFullName()
+        {
+            Console.WriteLine($"{_lastName} {_firstName}");
+        }
+
+        public string GetFullName() => _firstName + " " + _lastName;
+
+    }
+}
